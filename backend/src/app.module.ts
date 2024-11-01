2854247fec3debf00133a09c672ac1ed888e0565
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-11-01 05:14:43 +0100

    Creating backend folder structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
+import { CallModule } from './call/call.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, AuthModule, UserModule, ChatModule, CallModule, NotificationsModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
