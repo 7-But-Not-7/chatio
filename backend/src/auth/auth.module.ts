44a1318d0a8b4cc499bcfcea2f9888ae9c7240ff
Obycodez55 <obikoyaadebayo55@gmail.com>
2024-11-11 20:29:03 +0100

    Add JWT and session management to Auth module; update environment configuration

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,10 +4,24 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { BcrytpService } from './services/bcrypt.service';
 import { CryptoService } from './services/crypto.service';
+import { SessionModule } from 'src/session/session.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, BcrytpService, CryptoService],
-  imports: []
+  imports: [
+    UserModule,
+    SessionModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        global: true,
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
