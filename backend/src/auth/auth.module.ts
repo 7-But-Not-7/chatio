3935ae5a2bcf45e49d262b2a0981b3c85cf933d7
Obycodez55 <obikoyaadebayo55@gmail.com>
2024-11-11 17:01:41 +0100

    Add bcrypt and crypto services to Auth module for enhanced security

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { BcrytpService } from './services/bcrypt.service';
+import { CryptoService } from './services/crypto.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, BcrytpService, CryptoService],
+  imports: []
 })
 export class AuthModule {}
