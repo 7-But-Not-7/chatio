9749136b8e1f01de3326ee4c560c959821857dc4
Obycodez55 <obikoyaadebayo55@gmail.com>
2024-11-19 18:42:20 +0100

    Enhance sign-up step management; integrate Zustand persistence with localStorage

--- frontend/src/pages/auth/hooks/useSignUpStep.ts
+++ frontend/src/pages/auth/hooks/useSignUpStep.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { createLinkedList } from "../../../utils/create-linked-list";
 import { StepNode } from "../../../types";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface SignUpStepState {
     currentStep: StepNode<typeof steps[number]>; // The current step
@@ -14,19 +15,27 @@ const steps = ["sign-up", "otp", "profile", "success"];
 const initialStep = createLinkedList(steps);
 
 // Create the Zustand store with the necessary actions and state
-export const useSignUpStep = create<SignUpStepState>((set) => ({
-    currentStep: initialStep,
-    next: () => set((state) => {
-        if (state.currentStep.next) {
-            return { currentStep: state.currentStep.next };
+export const useSignUpStep = create(
+    persist<SignUpStepState>(
+        (set) => ({
+            currentStep: initialStep,
+            next: () => set((state) => {
+                if (state.currentStep.next) {
+                    return { currentStep: state.currentStep.next };
+                }
+                return state;
+            }),
+            prev: () => set((state) => {
+                if (state.currentStep.prev) {
+                    return { currentStep: state.currentStep.prev };
+                }
+                return state;
+            }),
+            reset: () => set({ currentStep: initialStep }), // Reset to the initial step
+        }),
+        {
+            name: "sign-up-step", // name of the item in the storage
+            storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
         }
-        return state;
-    }),
-    prev: () => set((state) => {
-        if (state.currentStep.prev) {
-            return { currentStep: state.currentStep.prev };
-        }
-        return state;
-    }),
-    reset: () => set({ currentStep: initialStep }), // Reset to the initial step
-}));
+    )
+);
