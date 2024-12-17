e740c791d2d6c89b73151a9ee8941fde46e9b49b
kennedyifeadi <kennedyifeadi@gmail.com>
2024-12-17 22:55:57 +0100

    Refactor service worker revision in sw.js

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fefb5367'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nv0f3fu3a5o"
+    "revision": "0.sbgh3sq9kug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
