f2718702dcd44f76b2d6d355d52ee99c87c6dc24
Obycodez55 <obikoyaadebayo55@gmail.com>
2024-12-30 14:42:36 +0100

    Add ChatGateway for WebSocket support; update ChatModule and add tests for gateway functionality

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -3,10 +3,11 @@ import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/chat.entity';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway],
   imports: [TypeOrmModule.forFeature([Message])],
 })
 export class ChatModule {}
