f19ac577ca726d9342b03f8ec1318e242e233bb7
Obycodez55 <obikoyaadebayo55@gmail.com>
2024-12-31 19:36:28 +0100

    Remove QueueModule dependency from notifications module

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { QueueModule } from 'src/queue/queue.module';
 import { NotificationsGateway } from './notifications.gateway';
 import { AuthModule } from 'src/auth/auth.module';
 import * as admin from 'firebase-admin';
