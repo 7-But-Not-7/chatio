6acae1b5356e93a8574483dd655308424bc40f4b
Obycodez55 <obikoyaadebayo55@gmail.com>
2025-01-11 11:09:58 +0100

    Update prebuild script to install dependencies and fix API URL in production config

--- backend/src/config/environment/production.ts
+++ backend/src/config/environment/production.ts
@@ -5,7 +5,7 @@ export default () => ({
     isProduction: true,
     debug: false,
     baseUrl: process.env.BASE_URL,
-    apiUrl: `${process.env.API_URL}/${AppEnum.API_PREFIX}`,
+    apiUrl: `${process.env.BASE_URL}/${AppEnum.API_PREFIX}`,
     clientUrl: process.env.CLIENT_URL || AppEnum.CLIENT_URL,
   });
   
