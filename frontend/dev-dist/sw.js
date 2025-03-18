24b5abe178f9a7209d496a10ffbcccb1d6884b2c
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-18 20:30:17 +0100

    Update service worker revision and add new dependencies for build tools

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fefb5367'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pk6et7fmlv"
+    "revision": "0.fa55kbpt15g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
